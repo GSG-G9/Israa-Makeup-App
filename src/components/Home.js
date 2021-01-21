0ee304c16b3e45a365f7bf21fcea49dba369a988
Israa91 <eng.israa.1991@gmail.com>
2021-01-21 14:19:35 +0200

    create category content route

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const Home = () => {
+  return (
+    <header className="App-header">
+      <h1>Israa Makeup API</h1>
+    </header>
+  );
+};
+export default Home;
---
+++ src/context/MakeupContext.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MakeupContext = React.createContext();
+
+export default MakeupContext;
